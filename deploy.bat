90a5982994abc682a44566ab914e692327d161b1
darskkaa <johnfdark@duck.com>
2025-05-14 16:58:04 -0400

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,7 @@
 
+@echo off
+start powershell -Command "cd C:\Users\"Your Location"\Downloads\stock-forecast-app\backend; python -m venv venv; .\venv\Scripts\Activate; pip install -r requirements.txt; uvicorn main:app --reload --port 8000"
+start powershell -Command "cd C:\Users\Users\"Your Location"\Downlaods\stock-forecast-app\frontend\stock-forecast-frontend; npm install; npm run dev"
+echo Stock Forecast Application is starting...
+echo Backend: http://localhost:8000
+echo Frontend: http://localhost:3000
